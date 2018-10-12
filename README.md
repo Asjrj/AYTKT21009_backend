771906602721a3d24f4cf97a036a23a1ae956291
Antti Penttinen <asjrj200@gmail.com>
2018-10-12 13:22:42 +0300

    Tehtävä 3.11

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 AYTKT21009 Tehtävät 3.1 -> 
 Tehtävän 3.10 Backend löytyy: https://puhback-yerghltpig.now.sh/api/persons/
+
+Tehtävän 3.11 sovellus löytyy: 
+https://puhback-bwjkctktmz.now.sh
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :data :res[content-length] - :response-time ms'))
